cd7a23f357aeef08c3e8dde584fc3b6e29df05e1
Paul Stearns <celestialcoyote@gmail.com>
2023-02-17 16:22:28 -0800

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const App = () => {
+
+    return (
+        <div className="app">
+            <p>stopwatch</p>
+        </div>
+    );
+};
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    entry: './index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', ',jsx', '.json', '.ts', '.tsx', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ],
+    devtool: 'eval-cheap-source-map',
+    devServer: {
+        port: '5000',
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        open: true,
+        hot: true,
+        liveReload: true,
+        historyApiFallback: true
+    }
+};
